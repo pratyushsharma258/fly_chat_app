61101d6d433e2801bd286b78bb9f03ae2d5154ee
PRATYUSH SHARMA <pratyushsharma258@gmail.com>
2023-06-10 22:30:51 +0530

    updated backend url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes.";
 
 function App() {
-  axios.defaults.baseURL = "http://localhost:4040";
+  axios.defaults.baseURL = "https://fly-chat.onrender.com";
   axios.defaults.withCredentials = true;
 
   return (
