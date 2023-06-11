ef73ed83515e2e70382ea78bbe4b13abd124f3ca
PRATYUSH SHARMA <pratyushsharma258@gmail.com>
2023-06-11 15:42:35 +0530

    added loader

--- client/src/RegisterAndLogin.jsx
+++ client/src/RegisterAndLogin.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { UserContext } from "./UserContext";
+import { RotatingLines } from "react-loader-spinner";
 
 function RegisterAndLogin() {
   const [username, setUsername] = useState("");
@@ -9,13 +10,16 @@ function RegisterAndLogin() {
   const { setUsername: setLoggedInUsername, setId } = useContext(UserContext);
   const [isLoginOrRegister, setIsLoginOrRegister] = useState("login");
   const [alertMessage, setAlertMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const passBoxRef = useRef();
   const userBoxRef = useRef();
   const confPassBoxRef = useRef();
 
   async function requestHandler() {
     const url = isLoginOrRegister === "register" ? "register" : "login";
+    setIsLoading(true);
     const { data } = await axios.post(url, { username, password });
+    setIsLoading(false);
     if (data === "invalid") {
       setAlertMessage("Wrong password");
       passBoxRef.current.value = "";
@@ -70,74 +74,94 @@ function RegisterAndLogin() {
 
   return (
     <div>
-      {alertMessage && (
-        <div
-          className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
-          role="alert"
-        >
-          <span className="font-bold">{alertMessage + "! "}</span>
-          <span>Please re-enter the credentials.</span>
-        </div>
-      )}
-      <div className="bg-[url(./assets/log_back.jpg)] bg-cover h-screen flex items-center">
-        <form action="" className="w-64 mx-auto mb-12" onSubmit={submitHandler}>
-          <input
-            type="text"
-            placeholder="username"
-            ref={userBoxRef}
-            className="bg-green-500 block w-full rounded-sm p-2 mb-2 border text-white placeholder-white"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required={true}
-          />
-          <input
-            type="password"
-            placeholder="password"
-            className="block w-full rounded-sm p-2 mb-2 border bg-green-500 text-white placeholder-white"
-            value={password}
-            ref={passBoxRef}
-            onChange={(e) => setPassword(e.target.value)}
-            required={true}
-          />
-          {isLoginOrRegister === "register" && (
+      <div
+        className={
+          "h-screen flex items-center justify-center bg-[url(./assets/log_back.jpg)] bg-cover " +
+          (isLoading ? "" : "hidden")
+        }
+      >
+        <RotatingLines
+          strokeColor="black"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="96"
+          visible={isLoading}
+        />
+      </div>
+      <div className={"flex-grow " + (isLoading ? "hidden" : "")}>
+        {alertMessage && (
+          <div
+            className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
+            role="alert"
+          >
+            <span className="font-bold">{alertMessage + "! "}</span>
+            <span>Please re-enter the credentials.</span>
+          </div>
+        )}
+        <div className="bg-[url(./assets/log_back.jpg)] bg-cover h-screen flex items-center">
+          <form
+            action=""
+            className="w-64 mx-auto mb-12"
+            onSubmit={submitHandler}
+          >
             <input
-              type="password"
-              placeholder="confirm password"
-              className="block w-full rounded-sm p-2 mb-2 border bg-green-500 text-white placeholder-white"
-              value={confPassword}
-              ref={confPassBoxRef}
-              onChange={(e) => setConfPassword(e.target.value)}
+              type="text"
+              placeholder="username"
+              ref={userBoxRef}
+              className="bg-green-500 block w-full rounded-sm p-2 mb-2 border text-white placeholder-white"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               required={true}
             />
-          )}
-          <button className="bg-amber-800 text-white block w-full rounded-sm p-2">
-            {isLoginOrRegister === "register" ? "Register" : "Login"}
-          </button>
-          <div className="text-white text-center mt-2">
+            <input
+              type="password"
+              placeholder="password"
+              className="block w-full rounded-sm p-2 mb-2 border bg-green-500 text-white placeholder-white"
+              value={password}
+              ref={passBoxRef}
+              onChange={(e) => setPassword(e.target.value)}
+              required={true}
+            />
             {isLoginOrRegister === "register" && (
-              <div>
-                Already a member?
-                <button
-                  className="ml-1"
-                  onClick={() => setIsLoginOrRegister("login")}
-                >
-                  Login here
-                </button>
-              </div>
+              <input
+                type="password"
+                placeholder="confirm password"
+                className="block w-full rounded-sm p-2 mb-2 border bg-green-500 text-white placeholder-white"
+                value={confPassword}
+                ref={confPassBoxRef}
+                onChange={(e) => setConfPassword(e.target.value)}
+                required={true}
+              />
             )}
-            {isLoginOrRegister === "login" && (
-              <div>
-                Don't have an account?
-                <button
-                  className="ml-1"
-                  onClick={() => setIsLoginOrRegister("register")}
-                >
-                  Register
-                </button>
-              </div>
-            )}
-          </div>
-        </form>
+            <button className="bg-amber-800 text-white block w-full rounded-sm p-2">
+              {isLoginOrRegister === "register" ? "Register" : "Login"}
+            </button>
+            <div className="text-white text-center mt-2">
+              {isLoginOrRegister === "register" && (
+                <div>
+                  Already a member?
+                  <button
+                    className="ml-1"
+                    onClick={() => setIsLoginOrRegister("login")}
+                  >
+                    Login here
+                  </button>
+                </div>
+              )}
+              {isLoginOrRegister === "login" && (
+                <div>
+                  Don't have an account?
+                  <button
+                    className="ml-1"
+                    onClick={() => setIsLoginOrRegister("register")}
+                  >
+                    Register
+                  </button>
+                </div>
+              )}
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
